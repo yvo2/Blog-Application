1713b385e3e68709880ad1fecb1508e8d14154a3
Lars Bärtschi <lars.baertschi@bluewin.ch>
2017-11-02 09:05:42 +0100

    + Added Repository superclass

---
+++ lib/Repository.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'Database.php';
+
+/**
+ * Repository base class
+ * comes with some helper methods
+ */
+class Repository {
+
+  /**
+   * TableName should be set by subclass
+   */
+   protected $tableName = null;
+
+   /**
+    * Read an entry by id and return all fields
+    */
+    public function getById($id) {
+      $query = "SELECT * FROM {$this->tableName} WHERE id=?";
+      $statement = Database::getConnection()->prepare($query);
+      $statement->bind_param('i', $id);
+      $statement->execute();
+      $result = $statement->get_result();
+      if (!$result) {
+          throw new Exception($statement->error);
+      }
+      $row = $result->fetch_object();
+      $result->close();
+      return $row;
+    }
+
+}
+
+ ?>
---
+++ repository/BlogRepository.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../lib/Repository.php';
+
+class BlogRepository extends Repository {
+
+  $this->tableName = "blog";
+
+}
+
+ ?>
