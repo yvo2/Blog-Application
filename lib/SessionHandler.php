80be6460a4655b498683cde4a7cd37a64fc09fa7
yvo2 <yvo.a.hofer@bluewin.ch>
2017-11-23 08:46:04 +0100

    SessionHandler

---
+++ lib/SessionHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../repository/UserRepository.php';
+
+class SessionHandler {
+
+    public function signInAsId($id) {
+      $_SESSION["userId"] = $id;
+    }
+
+    public function isSignedIn() {
+      return isset($_SESSION["userId"]);
+    }
+
+    public function getUser() {
+      if (!$this->isSignedIn()) {
+        $user = (object) array('signedIn' => false);
+        return $user;
+      }
+
+      $userRepository = new UserRepository();
+      $user = (object) $userRepository->readById($_SESSION["userId"]);
+      $user->signedIn = true;
+
+      return $user;
+    }
+}
+
+?>
