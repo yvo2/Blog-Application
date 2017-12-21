2d14377f1286d3216f8275ee7665283894d3d232
Lars Bärtschi <lars.baertschi@bluewin.ch>
2017-12-21 08:19:13 +0100

    + Optimized login validation (dynamic errors)

--- view/User_login.php
+++ view/User_login.php
@@ -1,22 +1,24 @@
 <?php
-if ($invalid) {
-  ?>
-  <div class="row" id="alert_box">
-    <div class="col s12 m12">
-      <div class="card red darken-1">
-        <div class="row">
-          <div class="col s12 m10">
-            <div class="card-content white-text">
-              Ein Fehler ist aufgetreten. Stimmt die Email / das Passwort?
+  if ($invalid) {
+    ?>
+    <div class="row" id="alert_box">
+      <div class="col s12 m12">
+        <div class="card red darken-1">
+          <div class="row">
+            <div class="col s12 m10">
+              <div class="card-content white-text">
+                <?php foreach ($validationErrors as $error) { ?>
+                  <p><?= $error ?></p>
+                <?php } ?>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  <?php
-}
-?>
+    <?php
+  }
+ ?>
 <div class="row row-centered">
     <form class="col input-form" method="post">
       <div class="row">
